08a571cb0ab060ec3cda8552042859d302a5695d
HuyHT130204 <huyht1302@gmail.com>
2025-09-21 13:15:13 +0700

    Fixed frontend

--- mobile/constants/api.js
+++ mobile/constants/api.js
@@ -1,2 +1,2 @@
-export const API_URL = "https://wallet-api-cxqp.onrender.com/api";
+export const API_URL = "https://expense-tracker-h5n3.onrender.com/api";
 // export const API_URL = "http://localhost:5001/api";
